5204d556ae240a3ee43c140b26a1a1db12251881
EONRaider <livewire_voodoo@protonmail.com>
2020-05-21 12:04:32 -0300

    Convert to Python 3, refactor and apply PEP 8
    * Replaced the deprecated 'optparse' module for 'argparse'
    * Restructured the CLI parser under the '__main__' scope. The
    main() function now reads arguments from argparse.
    * Changed file name, variables and functions for readability and
    conformity to PEP 8
    * Refactored print functions to use string interpolation
    * Minor reformatting

--- chapter02/ssh_command.py
+++ chapter02/ssh_command.py
@@ -1,33 +1,30 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import pexpect
 
-PROMPT = ['# ', '>>> ', '> ','\$ ']
+PROMPT = ['# ', '>>> ', '> ', '\$ ']
+
 
 def send_command(child, cmd):
     child.sendline(cmd)
     child.expect(PROMPT)
-    print child.before
+    print(child.before)
+
 
 def connect(user, host, password):
     ssh_newkey = 'Are you sure you want to continue connecting'
-    connStr = 'ssh ' + user + '@' + host
-    child = pexpect.spawn(connStr)
-    ret = child.expect([pexpect.TIMEOUT, ssh_newkey,\
-                        '[P|p]assword:'])
-    
-    if ret == 0:
-        print '[-] Error Connecting'
+    conn_str = f'ssh {user}@{host}'
+    child = pexpect.spawn(conn_str)
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
+
+    if not ret:
+        print('[-] Error Connecting')
         return
-    
-    if ret == 1:
+    else:
         child.sendline('yes')
-        ret = child.expect([pexpect.TIMEOUT, \
-                            '[P|p]assword:'])
-        if ret == 0:
-            print '[-] Error Connecting'
+        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+        if not ret:
+            print('[-] Error Connecting')
             return
-    
+
     child.sendline(password)
     child.expect(PROMPT)
     return child
@@ -41,6 +38,6 @@ def main():
     child = connect(user, host, password)
     send_command(child, 'cat /etc/shadow | grep root')
 
+
 if __name__ == '__main__':
     main()
-
