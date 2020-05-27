9c41da63c59580f2e67d0f64d2dc5137270325b3
EONRaider <livewire_voodoo@protonmail.com>
2020-05-27 12:03:20 -0300

    Convert to Python 3, refactor and apply PEP 8
    * Check notes on README.md under the "Refactoring" section

---
+++ chapter03/discover_networks.py
@@ -0,0 +1,60 @@
+import argparse
+import requests
+import winreg
+
+
+def val2addr(val):
+    addr = ''
+    print(val)
+    for ch in val:
+        print(ch)
+        addr += '%02x ' % ord(ch)
+    return addr.strip(' ').replace(' ', ':')[:17]
+
+
+def wigle_print(username, password, netid):
+    url = "https://api.wigle.net/api/v2/network/detail"
+    net_info = requests.get(url, auth=(username, password),
+                            params=netid)
+    net_info.raise_for_status()
+    response = net_info.json()
+
+    map_lat = response['results'][0]['trilat']
+    map_lon = response['results'][0]['trilong']
+    print(f'[-] Lat: {map_lat}, Lon: {map_lon}')
+
+
+def print_nets(username, password):
+    net = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\\NetworkList\Signatures\\Unmanaged"
+    key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, net)
+    print('\n[*] Networks You have Joined.')
+
+    for i in range(100):
+        try:
+            guid = winreg.EnumKey(key, i)
+            net_key = winreg.OpenKey(key, str(guid))
+            (n, addr, t) = winreg.EnumValue(net_key, 5)
+            (n, name, t) = winreg.EnumValue(net_key, 4)
+            mac_addr = val2addr(addr)
+            net_name = str(name)
+            print(f'[+] {net_name} {mac_addr}')
+            wigle_print(username, password, mac_addr)
+            winreg.CloseKey(net_key)
+        except IndexError:
+            break
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        usage='python3 discover_networks.py -u WIGLE_USERNAME '
+              '-p WIGLE_PASSWORD\n'
+              'Get your API credentials at https://wigle.net/account\n'
+              'Use your API Name as the WIGLE_USERNAME and the API Token as '
+              'the WIGLE_PASSWORD arguments')
+    parser.add_argument('-u', type=str, metavar='WIGLE_USERNAME', required=True,
+                        help='specify WiGLE username')
+    parser.add_argument('-p', type=str, metavar='WIGLE_PASSWORD', required=True,
+                        help='specify WiGLE password')
+    args = parser.parse_args()
+
+    print_nets(args.u, args.p)
