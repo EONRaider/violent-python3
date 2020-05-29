ba726195042b4b389052ec181f06a379a9672473
EONRaider <livewire_voodoo@protonmail.com>
2020-05-29 15:57:52 -0300

    Add context managers to database connections

--- chapter03/skype_parse.py
+++ chapter03/skype_parse.py
@@ -4,69 +4,69 @@ import os
 
 
 def print_profile(skype_db):
-    conn = sqlite3.connect(skype_db)
-    c = conn.cursor()
-    c.execute("SELECT fullname, skypename, city, country, \
-      datetime(profile_timestamp,'unixepoch') FROM Accounts;")
+    with sqlite3.connect(skype_db) as conn:
+        c = conn.cursor()
+        c.execute("SELECT fullname, skypename, city, country, \
+          datetime(profile_timestamp,'unixepoch') FROM Accounts;")
 
-    for row in c:
-        print('[*] -- Found Account --')
-        print(f'   [+] User           : {str(row[0])}')
-        print(f'   [+] Skype Username : {str(row[1])}')
-        print(f'   [+] Location       : {str(row[2])}, {str(row[3])}')
-        print(f'   [+] Profile Date   : {str(row[4])}')
+        for row in c:
+            print('[*] -- Found Account --')
+            print(f'   [+] User           : {str(row[0])}')
+            print(f'   [+] Skype Username : {str(row[1])}')
+            print(f'   [+] Location       : {str(row[2])}, {str(row[3])}')
+            print(f'   [+] Profile Date   : {str(row[4])}')
 
 
 def print_contacts(skype_db):
-    conn = sqlite3.connect(skype_db)
-    c = conn.cursor()
-    c.execute("SELECT displayname, skypename, city, country,\
-      phone_mobile, birthday FROM Contacts;")
+    with sqlite3.connect(skype_db) as conn:
+        c = conn.cursor()
+        c.execute("SELECT displayname, skypename, city, country,\
+          phone_mobile, birthday FROM Contacts;")
 
-    for row in c:
-        print('\n[*] -- Found Contact --')
-        print(f'   [+] User           : {str(row[0])}')
-        print(f'   [+] Skype Username : {str(row[1])}')
+        for row in c:
+            print('\n[*] -- Found Contact --')
+            print(f'   [+] User           : {str(row[0])}')
+            print(f'   [+] Skype Username : {str(row[1])}')
 
-        if str(row[2]) != '' and str(row[2]) != 'None':
-            print(f'   [+] Location       : {str(row[2])}, {str(row[3])}')
-        if str(row[4]) != 'None':
-            print(f'   [+] Mobile Number  : {str(row[4])}')
-        if str(row[5]) != 'None':
-            print(f'   [+] Birthday       : {str(row[5])}')
+            if str(row[2]) != '' and str(row[2]) != 'None':
+                print(f'   [+] Location       : {str(row[2])}, {str(row[3])}')
+            if str(row[4]) != 'None':
+                print(f'   [+] Mobile Number  : {str(row[4])}')
+            if str(row[5]) != 'None':
+                print(f'   [+] Birthday       : {str(row[5])}')
 
 
 def print_call_log(skype_db):
-    conn = sqlite3.connect(skype_db)
-    c = conn.cursor()
-    c.execute("SELECT datetime(begin_timestamp,'unixepoch'), \
-      identity FROM calls, conversations WHERE \
-      calls.conv_dbid = conversations.id;")
+    with sqlite3.connect(skype_db) as conn:
+        c = conn.cursor()
+        c.execute("SELECT datetime(begin_timestamp,'unixepoch'), \
+          identity FROM calls, conversations WHERE \
+          calls.conv_dbid = conversations.id;")
 
-    print('\n[*] -- Found Calls --')
+        print('\n[*] -- Found Calls --')
 
-    for row in c:
-        print(f'   [+] Time: {str(row[0])} | Partner: {str(row[1])}')
+        for row in c:
+            print(f'   [+] Time: {str(row[0])} | Partner: {str(row[1])}')
 
 
 def print_messages(skype_db):
-    conn = sqlite3.connect(skype_db)
-    c = conn.cursor()
-    c.execute("SELECT datetime(timestamp,'unixepoch'), \
-              dialog_partner, author, body_xml FROM Messages;")
-    print('\n[*] -- Found Messages --')
+    with sqlite3.connect(skype_db) as conn:
+        c = conn.cursor()
+        c.execute("SELECT datetime(timestamp,'unixepoch'), \
+                  dialog_partner, author, body_xml FROM Messages;")
+        print('\n[*] -- Found Messages --')
 
-    for row in c:
-        try:
-            if 'partlist' not in str(row[3]):
-                if str(row[1]) != str(row[2]):
-                    msg_direction = f'To {str(row[1])}: '
-                else:
-                    msg_direction = f'From {str(row[2])}: '
-                print(f'Time: {str(row[0])} {msg_direction} {str(row[3])}')
-        except Exception as e:
-            print(f'[-] Exception: {e}')
-            pass
+        for row in c:
+            try:
+                if 'partlist' not in str(row[3]):
+                    if str(row[1]) != str(row[2]):
+                        msg_direction = f'To {str(row[1])}: '
+                    else:
+                        msg_direction = f'From {str(row[2])}: '
+                    print(f'Time: {str(row[0])} {msg_direction} {str(row[3])}')
+            except Exception as e:
+                print(f'[-] Exception: {e}')
+                pass
 
 
 if __name__ == '__main__':
